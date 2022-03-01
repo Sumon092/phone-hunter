e89a675e235ff0b9b841ed8e4e7d11765fe9b33d
sumon092 <m.sumon@outlook.com>
2022-03-01 22:21:15 +0600

    first section complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const searchPhone = () => {
     const searchText = document.getElementById('input-field').value;
     loadPhone(searchText);
+
+    // clear search field
+    document.getElementById('input-field').value = '';
 }
 
 const loadPhone = searchText => {
@@ -12,24 +15,26 @@ const loadPhone = searchText => {
 const displayPhone = phones => {
     console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
+    const disPlay20items = phones.slice(0, 20);
     phoneContainer.textContent = '';
-    for (const phone of phones) {
+    for (const phone of disPlay20items) {
         const div = document.createElement('div');
-        div.innerHTML = `<div class="col">
+        div.innerHTML = `<div class="col shadow-lg p-3">
          <div class="card">
-             <img src="..." class="card-img-top" alt="...">
+             <img src="${phone.image}" class="card-img-top rounded" alt="...">
              <div class="card-body">
-                 <h5 class="card-title">${phone.brand}</h5>
-                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                     to
-                     additional content. This content is a little bit longer.</p>
+                 <h3 class="card-title fw-bold text-warning">${phone.phone_name}</h3>
+                 <h4 class="card-title">Brand : ${phone.brand}</h4>
              </div>
+             <button class="btn btn-primary btn-rounded fw-bold fs-4" onclick="exploreMore();">EXPLORE MORE</button>
          </div>
+         
      </div>`
+
         phoneContainer.appendChild(div);
     }
     /*  
  
  }) */
 }
-loadPhone('iphone');
+loadPhone('a');
